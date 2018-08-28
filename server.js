9df89e95f9fdc15668a8cf0a0f49a680870a59dc
LAPTOP-LBC3BOM6\juanc <juancamilo_11@utexas.edu>
2018-08-27 20:36:21 -0500

    testing this shit out

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const homeRoute = app.get(htmlRoutes.publicPaths.home, function(req, res) {
-  console.log(friendsArray);
   res.sendFile(path.join(__dirname, htmlRoutes.localPaths.home));
 });
 
