45bac50ac592535430545f56931e66b1ec0a2349
LAPTOP-LBC3BOM6\juanc <juancamilo_11@utexas.edu>
2018-09-02 14:44:55 -0500

    full send again

--- app/routing/htmlRouting.js
+++ app/routing/htmlRouting.js
@@ -6,7 +6,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 let homePath = app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "./app/public/homr.html"));
+  res.sendFile(path.join(__dirname, "./app/public/home.html"));
 });
 
 let surveyPath = app.get("/survey", (req, res) => {
