d96b9f3ca5e4fd1da3dfcb828c9ba85b5bf419bb
LAPTOP-LBC3BOM6\juanc <juancamilo_11@utexas.edu>
2018-09-02 15:02:51 -0500

    plssss

--- app/routing/htmlRouting.js
+++ app/routing/htmlRouting.js
@@ -8,14 +8,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 let homePath = app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "./app/public/home.html"));
+  res.sendFile(path.join(__dirname, "../public/home.html"));
 });
 
 let surveyPath = app.get("/survey", (req, res) => {
-  res.sendFile(path.join(__dirname, "./app/public/survey.html"));
+  res.sendFile(path.join(__dirname, "../public/survey.html"));
 });
 
 module.exports = {
-  homePath: homePath(),
-  surveyPath: surveyPath()
+  homePath: homePath,
+  surveyPath: surveyPath
 };
