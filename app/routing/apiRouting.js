d6face52bfb359c02679d43ed12b7d10cc897341
LAPTOP-LBC3BOM6\juanc <juancamilo_11@utexas.edu>
2018-09-02 15:23:05 -0500

    full send againnnnn

--- app/routing/apiRouting.js
+++ app/routing/apiRouting.js
@@ -1,7 +1,74 @@
-let apiPaths = {
-  route: "/api/friends",
-};
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const friendsArray = require("../data/friends.js");
+
+let app = express();
+
+//setting varible friends to represent the array of friends located in friends.js
+let friends = friendsArray.array;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+let readApi = app.get("../data/friends.js", (req, res) => {
+  return res.json(friends);
+});
+
+let writeApi = app.post("../data/friends.js", (req, res) => {
+
+  //this sets new Friend equal to the JSON object passed from the client
+  let newFriend = req.body;
+  //match will be the index of the person in the friends array the newly added person matches with
+  let match = "";
+  let sum = 0;
+  let newSum = 0;
+  //if the friends array is empty, we respond with a false
+  //this lets the front end know that the array is empty and there is no one
+  //the first user can match with
+  if (friends.length == 0) {
+    friends.push(newFriend);
+    res.send(false);
+  } else {
+    //map will go through each element in the friend array
+    //in the map callback we iterate through the scores array and compare each value to the newest array that was added
+    //the total difference is stored in sum
+    //need a way to
+    friends.map((x, index) => {
+      if (index === 0) {
+        for (i = 0; i < x.scores.length; i++) {
+          let diff = Math.abs(
+            parseInt(x.scores[i]) - parseInt(newFriend.scores[i])
+          );
+          sum = sum + diff;
+        }
+        //initialized match, if all other sums in the array are greater than the first person's
+        //the match will be the first person in the array
+        match = index;
+      } else {
+        for (i = 0; i < x.scores.length; i++) {
+          let diff = Math.abs(
+            parseInt(x.scores[i]) - parseInt(newFriend.scores[i])
+          );
+          newSum = newSum + diff;
+        }
+        //this compared the latest sum to the lowest sum prior
+        //if the new sum is lower than the new sum, sum (lowest) is replaced
+        //this also updates who the match is
+        if (newSum < sum) {
+          sum = newSum;
+          match = index;
+        }
+        newSum = 0;
+      }
+    });
+    friends.push(newFriend);
+    //the response sent back is the name of the person the user matched with
+    res.send(friends[match].name);
+  }
+});
 
 module.exports = {
-  apiPaths: apiPaths
+  apiRead: readApi,
+  apiWrite: writeApi
 };
--- server.js
+++ server.js
@@ -2,8 +2,6 @@
 // =============================================================
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require("path");
-const friends = require("./app/data/friends");
 const htmlRoutes = require("./app/routing/htmlRouting.js");
 const apiRoutes = require("./app/routing/apiRouting.js");
 
@@ -21,56 +19,10 @@ app.use(htmlRoutes.homePath);
 //survey page route and callback
 app.use(htmlRoutes.surveyPath);
 
-//routes are fine, need to figure out what is going on with the array
-app.get(apiRoutes.apiPaths.route, function(req, res) {
-  return res.json(friends.array);
-});
+app.use(apiRoutes.apiRead);
 
-//routes are fine
-//logic to compare most recent add to others in the array will go here
-app.post(apiRoutes.apiPaths.route, (req, res) => {
-  let newFriend = req.body;
-  //match will be the person the newly added array matches with
-  let match = "";
-  let sum = 0;
-  let newSum = 0;
-  //if the friends array is empty, we respond with a false
-  //this lets the front end know that the array is empty and there is no one
-  //the first user can match with
-  if (friends.array.length == 0) {
-    friends.array.push(newFriend);
-    res.send(false);
-  } else {
-    //map will go through each element in the friend array
-    //in the map callback we iterate through the scores array and compare each value to the newest array that was added
-    //the total difference is stored in sum
-    //need a way to
-    friends.array.map((x, index) => {
-      if (index === 0) {
-        for (i = 0; i < x.scores.length; i++) {
-          let diff = Math.abs(parseInt(x.scores[i]) - parseInt(newFriend.scores[i]));
-          sum = sum + diff;
-        }
-      } else {
-        for (i = 0; i < x.scores.length; i++) {
-          let diff = Math.abs(parseInt(x.scores[i]) - parseInt(newFriend.scores[i]));
-          newSum = newsum + diff;
-        }
-        //this compared the latest sum to the lowest sum prior
-        //if the new sum is lower than the new sum, sum (lowest) is replaced
-        //this also updates who the match is
-        if(newSum < sum){
-          sum = newSum;
-          match = index;
-        }
-        newSum = 0;
-      }
-    });
-    friends.array.push(newFriend);
-    //the response sent back is the name of the person the user matched with
-    res.send(friends.array[match].name);
-  }
-});
+app.use(apiRoutes.apiWrite);
+
 
 // Starts the server to begin listening
 // =============================================================
